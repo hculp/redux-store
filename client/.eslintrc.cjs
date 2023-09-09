794c52097519d1855d86f04f930174c962c4be49
Houston Culpepper <howacul@gmail.com>
2023-09-09 05:54:31 -0400

    retry package.json update

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,18 +4,23 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
     'airbnb',
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
+  plugins: ['react', 'react-refresh'],
   rules: {
     indent: ['error', 2],
     'arrow-parens': [2, 'as-needed'],
@@ -31,25 +36,28 @@ module.exports = {
         objects: 'only-multiline',
         imports: 'never',
         exports: 'never',
-      }
+      },
     ],
     'func-names': 0,
     'linebreak-style': 0,
     'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
-    'object-curly-newline': ['error', {
-      ObjectExpression: 'always',
-      ObjectPattern: {
-        multiline: true
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 5,
+        },
       },
-      ImportDeclaration: {
-        multiline: true,
-        minProperties: 3
-      },
-      ExportDeclaration: {
-        multiline: true,
-        minProperties: 5
-      }
-    }]
+    ],
   },
 };
