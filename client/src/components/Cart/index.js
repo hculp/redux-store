926fa16b194ffef9979606ee352468c88b547466
Houston Culpepper <howacul@gmail.com>
2023-09-09 03:32:44 -0400

    updated cart component to use redux store cart actions

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,31 +1,35 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
+// import the useDispatch and useSelector hooks from react-redux to handle store actions and state
+import { useDispatch, useSelector } from "react-redux";
 
-import { QUERY_CHECKOUT } from '../../graphql/queries';
+import { QUERY_CHECKOUT } from "../../graphql/queries";
+// import the cart actions from the redux store
+import { cartActions } from "../../store/cart.slice";
 
-import { useCartContext } from '../../context/CartContext';
+import CartItem from "../CartItem";
 
-import CartItem from '../CartItem';
+import { idbPromise } from "../../utils/helpers";
+import Auth from "../../utils/auth";
 
-import { idbPromise } from '../../utils/helpers';
-import Auth from '../../utils/auth';
+import "./style.css";
 
-import './style.css';
-
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 function Cart() {
-  const { cart, cartOpen, toggleCart, addMultipleToCart } = useCartContext();
+  // initialize the dispatch function and the cart and cartOpen state variables from the redux store
+  const dispatch = useDispatch();
+  const { cart, cartOpen } = useSelector((state) => state.cart);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     if (data) {
-      stripePromise.then(res => {
+      stripePromise.then((res) => {
         res.redirectToCheckout({
-          sessionId: data.checkout.session
+          sessionId: data.checkout.session,
         });
       });
     }
@@ -36,9 +40,14 @@ function Cart() {
     // https://beta.reactjs.org/learn/synchronizing-with-effects#fetching-data
     let ignore = false;
     async function getCart() {
-      const savedCart = await idbPromise('cart', 'get');
+      const savedCart = await idbPromise("cart", "get");
       if (!ignore) {
-        addMultipleToCart([...savedCart]);
+        // dispatch the addMultipleToCart action with the data from the idbPromise
+        dispatch(
+          cartActions.addMultipleToCart({
+            products: [...savedCart],
+          })
+        );
       }
     }
 
@@ -46,12 +55,19 @@ function Cart() {
       getCart();
     }
 
-    return () => { ignore = true; };
+    return () => {
+      ignore = true;
+    };
   }, [cart.length]);
+
+  // a toggleCart function that dispatches the toggleCart action
+  function toggleCart() {
+    dispatch(cartActions.toggleCart());
+  }
 
   function calculateTotal() {
     let sum = 0;
-    cart.forEach(item => {
+    cart.forEach((item) => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
@@ -60,7 +76,7 @@ function Cart() {
   function submitCheckout() {
     const productIds = [];
 
-    cart.forEach(item => {
+    cart.forEach((item) => {
       // eslint-disable-next-line no-plusplus
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
@@ -69,7 +85,7 @@ function Cart() {
 
     getCheckout({
       variables: {
-        products: productIds
+        products: productIds,
       },
     });
   }
@@ -92,18 +108,17 @@ function Cart() {
       <h2>Shopping Cart</h2>
       {cart.length ? (
         <div>
-          {cart.map(item => (
+          {cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
 
           <div className="flex-row space-between">
-            <strong>
-              Total: $
-              {calculateTotal()}
-            </strong>
+            <strong>Total: ${calculateTotal()}</strong>
 
             {Auth.loggedIn() ? (
-              <button type="button" onClick={submitCheckout}>Checkout</button>
+              <button type="button" onClick={submitCheckout}>
+                Checkout
+              </button>
             ) : (
               <span>(log in to check out)</span>
             )}
--- client/src/context/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CartContextProvider } from './CartContext';
-export { default as CategoriesContextProvider } from './CategoriesContext';
-export { default as ProductsContextProvider } from './ProductsContext';
