195e384227bf1124630385c061e4319614632dac
Houston Culpepper <howacul@gmail.com>
2023-09-09 03:42:23 -0400

    updated categorymenu component to use the redux store category actions

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,35 +1,58 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+// import the useDispatch and useSelector hooks from react-redux to handle store actions and state
+import { useDispatch, useSelector } from "react-redux";
 
-import { QUERY_CATEGORIES } from '../../graphql/queries';
+import { QUERY_CATEGORIES } from "../../graphql/queries";
+// import the category actions from the redux store
+import { categoryActions } from "../../store/category.slice";
 
-import { useCategoriesContext } from '../../context/CategoriesContext';
-
-import { idbPromise } from '../../utils/helpers';
+import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
-  const { categories, updateCategories, updateCurrentCategory } = useCategoriesContext();
+  // initialize the dispatch function and the categories and currentCategory state variables from the redux store
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state) => state.categories);
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     if (categoryData) {
-      updateCategories(categoryData.categories);
-      categoryData.categories.forEach(category => {
-        idbPromise('categories', 'put', category);
+      // dispatch the updateCategories action with the data from the idbPromise
+      dispatch(
+        categoryActions.updateCategories({
+          categories: categoryData.categories,
+        })
+      );
+      categoryData.categories.forEach((category) => {
+        idbPromise("categories", "put", category);
       });
     } else if (!loading) {
-      idbPromise('categories', 'get')
-        .then(savedCategories => updateCategories(savedCategories));
+      idbPromise("categories", "get").then((savedCategories) => {
+        // dispatch the updateCategories action with the data from the idbPromise
+        dispatch(
+          categoryActions.updateCategories({
+            categories: savedCategories,
+          })
+        );
+      });
     }
-  }, [categoryData, loading]);
+    // add dispatch to the dependency array so that state changees call useEffect again
+  }, [categoryData, loading, dispatch]);
 
-  const handleClick = categoryId => updateCurrentCategory(categoryId);
+  const handleClick = (categoryId) => {
+    // dispatch the updateCurrentCategory action when a category button is clicked
+    dispatch(
+      categoryActions.updateCurrentCategory({
+        currentCategory: categoryId,
+      })
+    );
+  };
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map(item => (
+      {categories.map((item) => (
         <button
           type="button"
           key={item._id}
