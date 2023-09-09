7ff3d2df6477cf0f3be8e9642d9db6d61c32e089
Houston Culpepper <howacul@gmail.com>
2023-09-09 03:35:58 -0400

    updated cartitem component to use redux store actions for cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,32 +1,50 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
 
-import { useCartContext } from '../../context/CartContext';
+import { cartActions } from "../../store/cart.slice";
 
-import { idbPromise } from '../../utils/helpers';
+import { idbPromise } from "../../utils/helpers";
 
 function CartItem({ item }) {
-  const { removeFromCart, updateCartQuantity } = useCartContext();
+  const dispatch = useDispatch();
 
-  const handleRemoveFromCart = itemToRemove => {
-    removeFromCart(itemToRemove._id);
-    idbPromise('cart', 'delete', {
-      ...itemToRemove
+  const removeFromCart = (itemToRemove) => {
+    // dispatch the removeFromCart action with the data from the idbPromise
+    dispatch(
+      cartActions.removeFromCart({
+        _id: itemToRemove._id,
+      })
+    );
+    idbPromise("cart", "delete", {
+      ...itemToRemove,
     });
   };
 
-  const onChange = e => {
+  const onChange = (e) => {
     const { value } = e.target;
-    if (value === '0') {
-      removeFromCart(item._id);
-      idbPromise('cart', 'delete', {
-        ...item
+    if (value === "0") {
+      // dispatch the removeFromCart action with the data from the idbPromise
+      dispatch(
+        cartActions.removeFromCart({
+          _id: item._id,
+        })
+      );
+      idbPromise("cart", "delete", {
+        ...item,
       });
     } else {
-      updateCartQuantity(item._id, parseInt(value, 10));
-      idbPromise('cart', 'put', {
-        ...item, purchaseQuantity: parseInt(value, 10)
+      // dispatch the updateCartQuantity action with the data from the idbPromise
+      dispatch(
+        cartActions.updateCartQuantity({
+          _id: item._id,
+          purchaseQuantity: parseInt(value, 10),
+        })
+      );
+      idbPromise("cart", "put", {
+        ...item,
+        purchaseQuantity: parseInt(value, 10),
       });
     }
   };
@@ -34,16 +52,11 @@ function CartItem({ item }) {
   return (
     <div className="flex-row">
       <div>
-        <img
-          src={`/images/${item.image}`}
-          alt=""
-        />
+        <img src={`/images/${item.image}`} alt="" />
       </div>
       <div>
         <div>
-          {item.name}
-          , $
-          {item.price}
+          {item.name}, ${item.price}
         </div>
         <div>
           <span>Qty:</span>
@@ -56,7 +69,7 @@ function CartItem({ item }) {
           <span
             role="img"
             aria-label="trash"
-            onClick={() => handleRemoveFromCart(item)}
+            onClick={() => removeFromCart(item)}
           >
             🗑️
           </span>
